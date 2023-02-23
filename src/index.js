d9d63db1af4015a395dc563210b89a44be18f7f9
Asal Etaati <85038893+asaletaati@users.noreply.github.com>
2023-02-23 16:35:57 +0330

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,36 @@
+function search() {
+  var apiKey = "5863935ee9cca4c02ed68203f807c65b";
+  var cityName = document.getElementById("search-box").value;
+  var tempUnit = document.querySelector('input[name="tempUnit"]:checked').value;
+
+  if(cityName !== "") {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${tempUnit}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      var weatherDetails = document.getElementById("weather-details");
+      weatherDetails.innerHTML = `City: ${data.name}<br>Temperature: ${data.main.temp}°<span id="temp-unit">${tempUnit === "metric" ? "C" : "F"}</span><br>Current time: ${new Date(data.dt * 1000).toLocaleString()}`;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  } else {
+    alert("Please enter city name!");
+  }
+}
+
+function currentLocation() {
+  var apiKey = "YOUR_API_KEY";
+  var tempUnit = document.querySelector('input[name="tempUnit"]:checked').value;
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=${tempUnit}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      var weatherDetails = document.getElementById("weather-details");
+      weatherDetails.innerHTML = `City: ${data.name}<br>Temperature: ${data.main.temp}°<span id="temp-unit">${tempUnit === "metric" ? "C" : "F"}</span><br>Current time: ${new Date(data.dt * 1000).toLocaleString()}`;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  });
+}
