a414b4c3669115b64134bf38ad76cac4e6af452e
Asal Etaati <85038893+asaletaati@users.noreply.github.com>
2023-03-02 15:58:08 +0330

    Rounding the temperatures

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ function search() {
   var tempUnit = document.querySelector('input[name="tempUnit"]:checked').value;
 
   if(cityName !== "") {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${tempUnit}&appid=${apiKey}`)
+    fetch(https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${tempUnit}&appid=${apiKey})
     .then(response => response.json())
     .then(data => {
       var weatherDetails = document.getElementById("weather-details");
-      weatherDetails.innerHTML = `City: ${data.name}<br>Temperature: ${data.main.temp}°<span id="temp-unit">${tempUnit === "metric" ? "C" : "F"}</span><br>Wind Speed: ${data.wind.speed} km/h<br>Current time: ${new Date(data.dt * 1000).toLocaleString()}`;
+      weatherDetails.innerHTML = City: ${data.name}<br>Temperature: ${Math.round(data.main.temp)}°<span id="temp-unit">${tempUnit === "metric" ? "C" : "F"}</span><br>Wind Speed: ${Math.round(data.wind.speed)} km/h<br>Current time: ${new Date(data.dt * 1000).toLocaleString()};
     })
     .catch(error => {
       console.error('Error:', error);
@@ -23,21 +23,14 @@ function currentLocation() {
   var tempUnit = document.querySelector('input[name="tempUnit"]:checked').value;
 
   navigator.geolocation.getCurrentPosition(function(position) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=${tempUnit}&appid=${apiKey}`)
+    fetch(https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=${tempUnit}&appid=${apiKey})
     .then(response => response.json())
     .then(data => {
       var weatherDetails = document.getElementById("weather-details");
-      weatherDetails.innerHTML = `City: ${data.name}<br>Temperature: ${data.main.temp}°<span id="temp-unit">${tempUnit === "metric" ? "C" : "F"}</span><br>Wind Speed: ${data.wind.speed} km/h<br>Current time: ${new Date(data.dt * 1000).toLocaleString()}`;
+      weatherDetails.innerHTML = City: ${data.name}<br>Temperature: ${Math.round(data.main.temp)}°<span id="temp-unit">${tempUnit === "metric" ? "C" : "F"}</span><br>Wind Speed: ${Math.round(data.wind.speed)} km/h<br>Current time: ${new Date(data.dt * 1000).toLocaleString()};
     })
     .catch(error => {
       console.error('Error:', error);
     });
   });
 }
-
-document.getElementById("search-box").addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    search();
-  }
-});
